e7485aa14424aef20bf045b5f4ee13f3200c746b
BYP000216_宮野真人 <m-miyano@za.biglobe.co.jp>
2017-06-26 17:52:03 +0900

    ２次配列の宣言部分実装(WIP)

--- pcc-algorithm3-kotlin/shortest_path_of_maze.kt
+++ pcc-algorithm3-kotlin/shortest_path_of_maze.kt
@@ -18,23 +18,38 @@ fun main(args: Array<String>){
   val start_m = 1
   val goal_n = 9
   val goal_m = 10
-  ShotestPathOfMaze().find_path(maze,n,m,start_n,start_m,goal_n,goal_m)
+  ShotestPathOfMaze(maze,n,m,start_n,start_m,goal_n,goal_m).find_path()
 }
 
 //迷路の最短路クラス
-class ShotestPathOfMaze(){
-  fun find_path(maze: Any, n: Int, m: Int, start_n: Int, start_m: Int, goal_n: Int, goal_m: Int){
+class ShotestPathOfMaze(val maze: Any, val n: Int, val m: Int, val start_n: Int, val start_m: Int, val goal_n: Int, val goal_m: Int){
+  fun find_path(){
+    //maze: Any, n: Int, m: Int, start_n: Int, start_m: Int, goal_n: Int, goal_m: Int
     //各マス毎に記録を残すための２次元配列を宣言
     var record = array2dOfNode(n,m)
-    //var stack = arrayOf<Array<CharArray>>()
+
+    var stack = arrayOfNulls<Coodinate?>(0)
+
+    //スタート地点のNodeを生成、対象のマスに保持させる。
     record[start_n-1][start_m-1] = Node(0,true)
-    println(record[1][0].cost)
+    stack = stack.plusElement(Pair(start_n-1,start_m-1))
+    println(stack[0])
   }
 
-  //２次元配列の宣言用function
-  fun array2dOfNode(sizeOuter: Int, sizeInner: Int): Array<Array<Any>>
-    = Array(sizeOuter){Array<Any>(sizeInner,{Node::class})}
+  //4方向のマスの値をチェックし、
+  fun check4directions(taget_n: Int, target_m: Int, record: Any){
+
+  }
+
+
+  //Nodeを持つ２次元配列の宣言用function
+  //Null safetyのためNode?を設定
+  fun array2dOfNode(sizeOuter: Int, sizeInner: Int): Array<Array<Node?>>
+    = Array(sizeOuter){arrayOfNulls<Node>(sizeInner)}
 }
 
-class Node(var cost: Int, var visited: Boolean){
+data class Node(var cost: Int, var visited: Boolean){
 }
+
+data class Coodinate(var c_n: Int, var c_m: Int)
+
