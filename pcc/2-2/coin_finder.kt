aed0097dd422abf1c5b1e0d1834b6cb79a9dce80
Masato <m.masato.m4.black@gmail.com>
2017-07-10 23:08:17 +0900

    所有するコインの数のロジックを追加

--- pcc/2-2/coin_finder.kt
+++ pcc/2-2/coin_finder.kt
@@ -1,33 +1,36 @@
 //main function
 fun main(args: Array<String>){
-  val price = 2177
-
-  CoinFinder().find_coins(price)
+  val in_pocket = arrayListOf(args[0].toInt(),args[1].toInt(),args[2].toInt(),args[3].toInt(),args[4].toInt(),args[5].toInt())
+  CoinFinder().find_coins(in_pocket, args[6].toInt())
 }
+
 class CoinFinder(){
-  fun find_coins(price: Int){
-    val coins = arrayListOf(500,100,50,10,5,1)
+  fun find_coins(in_pocket: ArrayList<Int>, price: Int){
+    var coins = in_pocket
+    val coinsList = arrayListOf(500,100,50,10,5,1)
     var answers = arrayListOf(0,0,0,0,0,0)
     var num = price
 
     // 値が大きい硬貨から順に評価する
-    for(i in 0..coins.size - 1){
+    for(i in 0..coinsList.size - 1){
       //各硬貨毎にbooleanをリセットする
       var bool = true
       // 対象硬貨で残金を割り算し、
       // 1以上の場合は対象硬貨を使用出来ると判断する。
       // 0以下の場合はループから抜ける。
       while(bool){
-        if(num / coins[i] <= 0){
+        if(num / coinsList[i] <= 0 || coins[i] == 0){
           bool = false
         } else{
+          coins[i] = coins[i] - 1
           answers[i] = answers[i] + 1
-          num = num - coins[i]
+          num = num - coinsList[i]
         }
       }
     }
     println("price: $price")
-    println("coin: $coins")
+    println("coinsList: $coinsList")
+    println("coins: $coins")
     println("answers: $answers")
   }
 }
