b937fb6000d3d43f173d4554790ddf9b8bbd88b1
BYP000216_宮野真人 <m-miyano@za.biglobe.co.jp>
2017-07-10 10:20:07 +0900

    ロジック一部修正：コメント追加

--- pcc/2-3/coin_finder.kt
+++ pcc/2-3/coin_finder.kt
@@ -10,18 +10,23 @@ class CoinFinder(){
     var answers = arrayListOf(0,0,0,0,0,0)
     var num = price
 
+    // 値が大きい硬貨から順に評価する
     for(i in 0..coins.size - 1){
+      //各硬貨毎にbooleanをリセットする
       var bool = true
+      // 対象硬貨で残金を割り算し、
+      // 1以上の場合は対象硬貨を使用出来ると判断する。
+      // 0以下の場合はループから抜ける。
       while(bool){
         if(num / coins[i] <= 0){
           bool = false
-        }
-        if(num / coins[i] != 0){
+        } else{
           answers[i] = answers[i] + 1
           num = num - coins[i]
         }
       }
     }
+    println("price: $price")
     println("coin: $coins")
     println("answers: $answers")
   }
